bb43ff838ab4ca36562a9fcacc0626453b8465f8
enusiraw <edlsiraw@gmail.com>
2024-08-14 05:31:34 -0900

    weather model added

---
+++ lib/model/weatherModel.dart
@@ -0,0 +1,43 @@
+class WeatherModel {
+  final String cityName;
+  final String description;
+  final double temperature;
+  final double feelsLike;
+  final double minTemperature;
+  final double maxTemperature;
+  final int humidity;
+  final double windSpeed;
+  final int pressure;
+  final int visibility;
+  final String weatherIcon;
+
+  WeatherModel({
+    required this.cityName,
+    required this.description,
+    required this.temperature,
+    required this.feelsLike,
+    required this.minTemperature,
+    required this.maxTemperature,
+    required this.humidity,
+    required this.windSpeed,
+    required this.pressure,
+    required this.visibility,
+    required this.weatherIcon,
+  });
+
+  factory WeatherModel.fromJson(Map<String, dynamic> json) {
+    return WeatherModel(
+      cityName: json['name'],
+      description: json['weather'][0]['description'],
+      temperature: json['main']['temp'].toDouble(),
+      feelsLike: json['main']['feels_like'].toDouble(),
+      minTemperature: json['main']['temp_min'].toDouble(),
+      maxTemperature: json['main']['temp_max'].toDouble(),
+      humidity: json['main']['humidity'],
+      windSpeed: json['wind']['speed'].toDouble(),
+      pressure: json['main']['pressure'],
+      visibility: json['visibility'],
+      weatherIcon: json['weather'][0]['icon'],
+    );
+  }
+}
